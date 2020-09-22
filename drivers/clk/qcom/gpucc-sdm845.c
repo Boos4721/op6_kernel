5579d08dce275d3314862c6cd72995d340388677
boos4721 <3.1415926535boos@gmail.com>
2020-09-22 18:53:59 +0800

    Revert "cpufreq: gpu oc to 835Mhz and uc to 150Mhz"
    This reverts commit 8acd8eb9c551255381866759fff74f125cc7f8bf.

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -274,7 +274,7 @@ static const struct freq_tbl ftbl_gpu_cc_gx_gfx3d_clk_src[] = {
 };
 
 static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
-	F(150000000, P_CRC_DIV,  1, 0, 0),
+	F(130000000, P_CRC_DIV,  1, 0, 0),
 	F(257000000, P_CRC_DIV,  1, 0, 0),
 	F(342000000, P_CRC_DIV,  1, 0, 0),
 	F(414000000, P_CRC_DIV,  1, 0, 0),
@@ -282,7 +282,7 @@ static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(596000000, P_CRC_DIV,  1, 0, 0),
 	F(675000000, P_CRC_DIV,  1, 0, 0),
 	F(710000000, P_CRC_DIV,  1, 0, 0),
-	F(835000000, P_CRC_DIV,  1, 0, 0),
+	F(810000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
 
